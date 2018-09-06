8fa4a7c73c0edd3e4728ac3007bc39d07cc193e5
Davíð Örn Jóhannsson @davideagle <davideaglephotos@gmail.com>
2018-09-06 23:53:53 +0000

    added kodi user

--- manifests/kodi/setup.pp
+++ manifests/kodi/setup.pp
@@ -1,3 +1,13 @@
 class mediacenter::kodi::setup {
   class { "kodi" : }
+
+  user { 'kodi':
+    comment => 'First Last',
+    home    => '/var/lib/kodi',
+    ensure  => present,
+    uid     => '420',
+    groups  => ['audio','video']
+    shell   => '/usr/bin/nologin kodi'
+  }
+
 }
