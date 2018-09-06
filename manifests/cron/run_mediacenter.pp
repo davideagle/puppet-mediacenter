3822285fee257c03714654194abaa2b0312713a7
Davíð Örn Jóhannsson @davideagle <davideaglephotos@gmail.com>
2018-09-06 20:01:47 +0000

    fixed path

--- manifests/cron/run_mediacenter.pp
+++ manifests/cron/run_mediacenter.pp
@@ -39,6 +39,6 @@ class mediacenter::cron::run_mediacenter {
   cron::job { 'puppetrun_every5min':
     minute  => '*/5',
     user    => 'root',
-    command => "sleep `perl -e 'print int(rand(300));'` && puppet apply --logdest syslog /home/vagrant/puppet-mediacenter/${module_name}/manifests/mediacenter.pp",
+    command => "sleep `perl -e 'print int(rand(300));'` && puppet apply --logdest syslog /etc/puppet/modules/${module_name}/manifests/mediacenter.pp",
   }
 }
