d681c948a0aad6dee08ad4ccbd8639767fd1d060
Davíð Örn Jóhannsson @davideagle <davideaglephotos@gmail.com>
2018-09-08 09:55:03 +0000

    added sickbeard user

--- manifests/sickbeard/setup.pp
+++ manifests/sickbeard/setup.pp
@@ -1,3 +1,11 @@
 class mediacenter::sickbeard::setup {
+
+  user { 'sickbeard':
+    comment => 'sickbeard',
+    home    => '/home/sickbeard/',
+    ensure  => present,
+    before  => Class['sickbeard']
+  }
+
   class { 'sickbeard': }
 }
