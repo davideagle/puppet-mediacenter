06252709c0ecf696793aaf31980e6432b36971a5
Davíð Örn Jóhannsson @davideagle <davideaglephotos@gmail.com>
2018-09-10 23:21:48 +0000

    adding mono-devel

--- manifests/jacket/setup.pp
+++ manifests/jacket/setup.pp
@@ -22,5 +22,10 @@ class mediacenter::jacket::setup {
       'id'     => '3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF',
       'server' => 'keyserver.ubuntu.com',
     },
+    before   => package['mono-devel'],
+  }
+
+  package { 'mono-devel':
+    ensure => installed,
   }
 }
