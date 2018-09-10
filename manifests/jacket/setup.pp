a4d5c9711f4b9e203f275a28932ba99188a3ae0d
Davíð Örn Jóhannsson @davideagle <davideaglephotos@gmail.com>
2018-09-10 23:20:39 +0000

    adding mono repo

--- manifests/jacket/setup.pp
+++ manifests/jacket/setup.pp
@@ -13,4 +13,14 @@ class mediacenter::jacket::setup {
   package { 'libmono-addins-msbuild-cil-dev':
     ensure => installed,
   }
+
+  include ::apt
+  apt::source { 'mono-official':
+    location => 'https://download.mono-project.com/repo/ubuntu',
+    repos    => 'main',
+    key      => {
+      'id'     => '3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF',
+      'server' => 'keyserver.ubuntu.com',
+    },
+  }
 }
