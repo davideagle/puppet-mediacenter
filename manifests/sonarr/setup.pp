c8ca922319e263e6bb578d2759080d9954efee66
Davíð Örn Jóhannsson @davideagle <davideaglephotos@gmail.com>
2018-09-12 09:36:41 +0000

    moving to docker

--- manifests/sonarr/setup.pp
+++ manifests/sonarr/setup.pp
@@ -1,5 +1,7 @@
 class mediacenter::sonarr::setup {
 
+  include ::docker
+
   docker::image { 'linuxserver/sonarr':
     ensure    => 'present',
     image_tag => 'latest'
@@ -40,35 +42,6 @@ class mediacenter::sonarr::setup {
     ensure => directory,
     mode   => '0600',
     owner  => 'sonarr'
-  }
-
-  class { 'sonarr':
-    package_ensure => present,
-    service_name   => 'sonarr',
-    service_enable => true,
-    service_ensure => running,
-    service_manage => true,
-    user           => 'sonarr',
-    group          => 'sonarr'
-  }
-
-  package { 'libmono-cil-dev':
-    ensure => installed,
-  }
-
-  package { 'mono-devel':
-    ensure => installed,
-  }
-
-  include ::apt
-  apt::source { 'mono-official':
-    location => 'https://download.mono-project.com/repo/ubuntu',
-    repos    => 'main',
-    key      => {
-      'id'     => '3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF',
-      'server' => 'keyserver.ubuntu.com',
-    },
-    before   => Package['mono-devel'],
   }
 
 }
